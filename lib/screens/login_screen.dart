816e1c3db37cf26c90581130a133b48851f7f147
vk26-create <villarojoaldrick26@gmail.com>
2026-03-05 15:20:58 +0800

    Create login_screen.dart with authentication UI

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,52 +1,30 @@
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatefulWidget {
-  @override
-  _LoginScreenState createState() => _LoginScreenState();
-}
-
-class _LoginScreenState extends State<LoginScreen> {
-  final _emailController = TextEditingController();
-  final _passwordController = TextEditingController();
-
-  void _login() {
-    final email = _emailController.text;
-    final password = _passwordController.text;
-    // Here you can add your authentication logic
-    print('Email: $email, Password: $password');
-  }
-
-  void _signUp() {
-    // Navigate to sign-up screen or handle sign-up logic
-    print('Navigate to sign-up screen');
-  }
-
+class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Login')), 
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             TextField(
-              controller: _emailController,
               decoration: InputDecoration(labelText: 'Email'),
             ),
             TextField(
-              controller: _passwordController,
               decoration: InputDecoration(labelText: 'Password'),
               obscureText: true,
             ),
             SizedBox(height: 20),
             ElevatedButton(
-              onPressed: _login,
+              onPressed: () {
+                // Add authentication logic here
+              },
               child: Text('Login'),
-            ),
-            TextButton(
-              onPressed: _signUp,
-              child: Text('Sign Up'),
             ),
           ],
         ),
